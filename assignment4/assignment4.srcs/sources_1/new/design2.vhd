ecf9a5785737c43b2bfe69dabacfeeb4032cedc3
Sindre Nordtveit <sindex82@gmail.com>
2023-09-21 10:20:29 +0200

    Added src files for task 3

---
+++ assignment4/assignment4.srcs/sources_1/new/design2.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/21/2023 09:25:19 AM
+-- Design Name: 
+-- Module Name: design2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+architecture design2 of assignment5 is
+    begin
+        process (clk, reset) is
+            variable t : std_ulogic;
+        begin
+            if (reset = ‘1’) then
+                t := '0';
+                y <= ‘0’;
+            elsif (rising_edge(clk)) then
+                t := a xor b;
+                y <= t;
+            end if;
+        end process;
+    end architecture;
